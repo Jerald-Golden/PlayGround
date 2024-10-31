3bb680d7e18cdc6b3cb166cf9d9dc76256cc2ba1
Jerald Golden <jeraldgolden00@gmail.com>
2024-10-31 08:18:32 +0530

    warning fixed

--- src/Environment/Map.js
+++ src/Environment/Map.js
@@ -2,13 +2,13 @@ import React from 'react';
 import MiniGames from '../Maps/MiniGames/MiniGames';
 import Level1 from '../Maps/Level1/Level1';
 import Level2 from '../Maps/Level2/Level2';
-import Clock_Tower from '../Maps/Clock Tower/ClockTower';
+import ClockTower from '../Maps/Clock Tower/ClockTower';
 
 const Map = ({ mapType }) => {
     return (
         <>
             {mapType === "MiniGames" && <MiniGames />}
-            {mapType === "Clock_Tower" && <Clock_Tower />}
+            {mapType === "Clock_Tower" && <ClockTower />}
             {mapType === "Level1" && <Level1 />}
             {mapType === "Level2" && <Level2 />}
         </>
